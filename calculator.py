2790af9139caf07f95b401477d1311520a4c308f
Malak | مبرمجة AI <malak.codes.iq@gmail.com>
2026-06-27 01:03:33 +0300

    Add calculator file

---
+++ calculator.py
@@ -0,0 +1,10 @@
+def add(x, y):
+        return x + y
+
+    def subtract(x, y):
+        return x - y
+
+    print("حاسبة بسيطة")
+    num1 = float(input("ادخل اول رقم: "))
+    num2 = float(input("ادخل ثاني رقم: "))
+    print("النتيجة:", add(num1, num2))
